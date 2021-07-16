450494f5a81424aa8a3e5b55e5a97f549008cf99
Aliyama <alwiarif72@yahoo.com>
2021-07-16 07:45:47 +0700

    Update Default Value

--- src/Customer.php
+++ src/Customer.php
@@ -10,7 +10,7 @@ class Customer{
          
      }
 
-     public function sayHello(string $name):string {
+     public function sayHello(string $name = "Test"):string {
          return "Hello $name, My name is $this->name";
      }
 
