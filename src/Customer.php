3234f37d1bda1bc183a830f69fd75a1e9cee04b4
Aliyama <alwiarif72@yahoo.com>
2021-07-16 07:30:03 +0700

    Belajar buat library

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aliyama\Belajar;
+
+class Customer{
+
+     public function __construct(private string $name)
+     {
+
+         
+     }
+
+     public function sayHello(string $name):string {
+         return "Hello $name, My name is $this->name";
+     }
+
+}
